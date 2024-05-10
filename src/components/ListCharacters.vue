c77cd00ed62b96a5b5b32f5172079f023348b724
Dvsteven <stevenhenao81@gmail.com>
2024-05-09 20:28:32 -0500

    tarjetas y estado actual

---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,40 @@
+<template>
+	<section>
+	  <div class="characters">
+		<div class="characters__item" v-for="character in characters" :key="character.id">
+		  <CardCharacter :character="character" />
+		</div>
+	  </div>
+	</section>
+  </template>
+
+
+<script>
+import { computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+
+import CardCharacter from '@/components/CardCharacter'
+
+export default {
+	components: {
+		CardCharacter
+	},
+	SetUp(){
+		const store = useStore()
+		const characters = computed(() => {
+			return store.state.charactersFilter
+		})
+		onUnmounted(() => {
+			store.dispatch('getCharacters')
+		})
+
+		return{
+			characters
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
